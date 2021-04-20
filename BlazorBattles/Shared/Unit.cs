310439c690d2e3080b02007b34c66587eb07ad7b
ishmam.anwar96 <ishmam.anwar96@hotmail.com>
2021-04-20 17:59:35 -0600

    Unit and User Service created

--- BlazorBattles/Shared/Unit.cs
+++ BlazorBattles/Shared/Unit.cs
@@ -6,11 +6,14 @@ namespace BlazorBattles.Shared
 {
     public class Unit
     {
+
         public int Id { get; set; }
         public string Title { get; set; }
         public int Attack { get; set; }
         public int Defense { get; set; }
         public int HitPoints { get; set; } = 100;
         public int BananaCost { get; set; }
+
+
     }
 }
