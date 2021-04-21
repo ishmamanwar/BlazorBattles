04acbe7c234dd8126401054ababdd77ceeb5d315
ishmam.anwar96 <ishmam.anwar96@hotmail.com>
2021-04-20 19:31:13 -0600

    UserLogin model created

---
+++ BlazorBattles/Shared/UserLogin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlazorBattles.Shared
+{
+    public class UserLogin
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
